f52954bc998fb80601e7d6af83a1602b4264f065
menor <josemenor@gmail.com>
2016-06-28 08:21:22 +0200

    Add a test component boilerplate

---
+++ src/testComponent/testComponent.js
@@ -0,0 +1,15 @@
+import React, {Component, PropTypes} from 'react'
+
+class TestComponent extends Component {
+  static propTypes = {
+    text: PropTypes.string
+  }
+
+  render() {
+    return (
+      <h1>{this.props.text}</h1>
+    )
+  }
+}
+
+export default TestComponent
