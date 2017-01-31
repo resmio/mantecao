9ef4f0676d60a9b9d0463a6a8f65e55f8d7aacc9
Steven Hein <steven.c.hein@gmail.com>
2017-01-31 17:29:05 +0100

    Add new icons

---
+++ src/icons/FinishedIcon.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Icon } from '../components/Icon'
+
+const FinishedIcon = (props) => (
+  <Icon { ...props }>
+    <path d="M22.2 7.9C21.2 7.2 20 6.7 18.7 6.3 17.8 6.1 16.9 6 16 6 9.9 6 5 10.9 5 17 5 23.1 9.9 28 16 28L16 28C22.1 28 27 23.1 27 17M14 13.1L18.4 17.6C18.7 17.9 19.4 17.9 19.8 17.6L29.1 8"/>
+  </Icon>
+)
+
+export default FinishedIcon
