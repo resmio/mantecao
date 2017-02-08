bf9184033b07f344a81bd4b4ebfd6b51c853c914
Steven Hein <steven.c.hein@gmail.com>
2017-02-08 11:23:54 +0100

    Add dropdown icon

---
+++ src/icons/DotDotDotIcon.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Icon } from '../components/Icon'
+
+const DotDotDotIcon = (props) => (
+  <Icon { ...props }>
+    <path d='M7 14L11 14 11 18 7 18 7 14ZM14 14L18 14 18 18 14 18 14 14ZM21 14L25 14 25 18 21 18 21 14Z' />
+  </Icon>
+)
+
+export default DotDotDotIcon
