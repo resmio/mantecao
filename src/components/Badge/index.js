87035312c9291f3dd0bb437c0a5296c06a585575
menor <josemenor@gmail.com>
2017-03-03 14:00:54 +0100

    Add a story for the badge
    Need to remove the index /Badge logic from the Badge folder for it to work

--- src/components/Badge/index.js
+++ src/components/Badge/index.js
@@ -1,1 +1,35 @@
-export { Badge } from './Badge'
+import React, {PropTypes} from 'react'
+import {colors} from '../../variables'
+
+const defaultBadgeStyle = {
+  display: 'inline-block',
+  minWidth: '10px',
+  padding: '1px 7px',
+  fontSize: '12px',
+  fontWeight: 700,
+  color: colors.white,
+  verticalAlign: 'bottom',
+  whiteSpace: 'nowrap',
+  textAlign: 'center',
+  backgroundColor: colors.dustyGray,
+  borderRadius: '10px'
+}
+
+/**
+ * small badge - we can update this later
+ */
+const Badge = (props) => {
+  const computedBadgeStyle = Object.assign({}, defaultBadgeStyle, props.style)
+  return (
+    <span style={computedBadgeStyle}>
+      {props.children}
+    </span>
+  )
+}
+
+Badge.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  style: PropTypes.object
+}
+
+export default Badge
--- src/stories/index.js
+++ src/stories/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { storiesOf, action, linkTo } from '@kadira/storybook';
-import Button from './Button';
+// import Alert from '../components/Alert';
+import Badge from '../components/Badge';
 import Welcome from './Welcome';
 
 storiesOf('Welcome', module)
@@ -8,10 +9,18 @@ storiesOf('Welcome', module)
     <Welcome showApp={linkTo('Button')}/>
   ));
 
-storiesOf('Button', module)
-  .add('with text', () => (
-    <Button onClick={action('clicked')}>Hello Button</Button>
-  ))
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>
+storiesOf('Badge', module)
+  .add('with some text', () => (
+    <Badge>Hola</Badge>
   ));
+
+// storiesOf('Alert', module)
+//   .add('error', () => (
+//     <Alert isOpen type="error" heading="example alert" message="some error" />
+//   ))
+//   .add('warning', () => (
+//     <Alert isOpen type="warning" heading="example alert" message="something important" />
+//   ))
+//   .add('with onRemove', () => (
+//     <Alert isOpen type="error" heading="example alert" message="something important" onRemove={action('removed')} />
+//   ));
