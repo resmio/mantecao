10c3c8162c97cea7d6bdb1ca2b4b0b01493b7f2d
menor <josemenor@gmail.com>
2017-06-15 09:16:52 +0200

    Try to output the modules without transforming to commonjs It fails when importing the modules in the browser Somehow we don't get the import/export built file we want in index.es.js Checked material-ui to see how they do it, although I'm missing something Keeping this here to give it another try soon.

--- tools/mantecao-babel-preset-es2015.js
+++ tools/mantecao-babel-preset-es2015.js
@@ -2,6 +2,9 @@ const es2015 = require('babel-preset-es2015').buildPreset
 
 const ENV = process.env.BABEL_ENV
 
+// We need to disable the modules option when compiling for ES6
+// This comes included in the es-2015 babel-preset
+// That's why we need to disable it explicitly
 module.exports = {
   presets: [
     [ es2015, {
