339499d16249f67bf5062161511c7c55224d4ec9
menor <josemenor@gmail.com>
2017-06-15 11:23:52 +0200

    (Bug) Fix the icon export

--- src/index.js
+++ src/index.js
@@ -1,27 +1,5 @@
-// components
-export Alert from './components/Alert'
-export Badge from './components/Badge'
-export BorderButton from './components/BorderButton'
-export Button from './components/Button'
-export ButtonGroup from './components/ButtonGroup'
-export Checkbox from './components/Checkbox'
-export ComboBox from './components/ComboBox'
-export DateSelector from './components/DateSelector'
-export DialogBody from './components/DialogBody'
-export DialogHeader from './components/DialogHeader'
-export Dropdown from './components/Dropdown'
-export Icon from './components/Icon'
-export InlineAlert from './components/InlineAlert'
-export KeyCodeListener from './components/KeyCodeListener'
-export Label from './components/Label'
-export SelectField from './components/SelectField'
-export Tag from './components/Tag'
-export TagCloud from './components/TagCloud'
-export TextField from './components/TextField'
-export UnstyledButton from './components/UnstyledButton'
-
-// icons
-// export * from './icons'
+export * from './components'
+export * from './icons'
 
 // variables
 export {colors, breakpoints} from './variables'
