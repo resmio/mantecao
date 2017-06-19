8865786070bb5505d511876a59ca73d6b1829ddd
menor <josemenor@gmail.com>
2017-06-19 10:51:54 +0200

    Add a prepush test task

--- src/components/Badge/Badge.js
+++ src/components/Badge/Badge.js
@@ -1,13 +1,18 @@
 import React, { PropTypes } from 'react'
 import { colors } from '../../variables'
 
-const defaultBadgeStyle = { display: 'inline-block', minWidth: '10px', padding: '1px 7px', fontSize: '12px', fontWeight: 700,
+const defaultBadgeStyle = {
+  display: 'inline-block',
+  minWidth: '10px',
+  padding: '1px 7px',
+  fontSize: '12px',
+  fontWeight: 700,
   color: colors.white,
   verticalAlign: 'bottom',
   whiteSpace: 'nowrap',
   textAlign: 'center',
   backgroundColor: colors.dustyGray,
-  borderRadius: '10px',
+  borderRadius: '10px'
 }
 
 /**
