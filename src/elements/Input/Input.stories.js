981c9b816604eac1ebcdbfcce1bcc06864886697
menor <josemenor@gmail.com>
2017-06-20 14:31:14 +0200

    Fix messed up merge

--- src/elements/Input/Input.stories.js
+++ src/elements/Input/Input.stories.js
@@ -1,48 +1,15 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
 import Input from './Input'
 
 storiesOf('Input', module)
-  .add('default', () => (
-    <Input placeholder='email@email.com'/>
-<<<<<<< HEAD
-=======
-  ))
-  .add('disabled', () => (
-    <Input disabled/>
-  ))
-  .add('with extra styling', () => (
-    <Input style={{backgroundColor:'fuchsia'}}/>
->>>>>>> Add stories to the Input element
-  ))
-<<<<<<< HEAD
-  .add('disabled', () => (
-    <Input disabled/>
-  ))
-  .add('with extra styling', () => (
-    <Input style={{backgroundColor:'fuchsia'}}/>
-  ))
-=======
->>>>>>> Make the Input work for different types
-  .add('type="password"', () => (
-    <Input type='password'/>
-  ))
-  .add('type="checkbox"', () => (
-    <Input type='checkbox'/>
-<<<<<<< HEAD
-  ))
-  .add('type="radio"', () => (
-    <Input type='radio'/>
-  ))
-  .add('type="file"', () => (
-    <Input type='file'/>
-=======
->>>>>>> Make the Input work for different types
-  ))
-  .add('type="radio"', () => (
-    <Input type='radio'/>
-  ))
-  .add('type="file"', () => (
-    <Input type='file'/>
-  ))
+  .add('default', () => <Input placeholder="email@email.com" />)
+  .add('disabled', () => <Input disabled />)
+  .add('with extra styling', () =>
+    <Input style={{ backgroundColor: 'fuchsia' }} />
+  )
+  .add('type="password"', () => <Input type="password" />)
+  .add('type="checkbox"', () => <Input type="checkbox" />)
+  .add('type="radio"', () => <Input type="radio" />)
+  .add('type="file"', () => <Input type="file" />)
