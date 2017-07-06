979ecbcca465f9a27e721a206307c556a143249d
menor <josemenor@gmail.com>
2017-07-06 10:54:55 +0200

    Add Stepper skeleton

---
+++ src/Stepper/Stepper.stories.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import Stepper from './Stepper'
+
+storiesOf('Stepper', module).add('default', () => <Stepper>Hello 💩</Stepper>)
