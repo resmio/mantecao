051a27c699d39e70bd3569894e52c3790566d5a4
menor <josemenor@gmail.com>
2017-07-07 08:05:59 +0200

    Add increase and decrease buttons

--- src/Stepper/Stepper.js
+++ src/Stepper/Stepper.js
@@ -1,17 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Stepper = ({ children, onClickAction }) => {
+const Stepper = ({
+  children,
+  onClickAction,
+  onDecreaseClick,
+  onIncreaseClick
+}) => {
   return (
     <div onClick={onClickAction}>
       {children}
+      <span id="mantecao-stepper-inc" onClick={onIncreaseClick}>+</span>
+      <span id="mantecao-stepper-dec" onClick={onDecreaseClick}>+</span>
     </div>
   )
 }
 
 Stepper.PropTypes = {
   children: PropTypes.node.isRequired,
-  onClickAction: PropTypes.function
+  onClickAction: PropTypes.function,
+  onIncreaseClick: PropTypes.function,
+  onDecreaseClick: PropTypes.function
 }
 
 export default Stepper
--- src/Stepper/Stepper.spec.js
+++ src/Stepper/Stepper.spec.js
@@ -4,8 +4,24 @@ import { shallow } from 'enzyme'
 import Stepper from './Stepper'
 
 const ON_CLICK = jest.fn()
+const INCREASE_ACTION = jest.fn()
+const DECREASE_ACTION = jest.fn()
 
 describe('Stepper', () => {
+  let stepper
+
+  beforeEach(() => {
+    stepper = shallow(
+      <Stepper
+        onClickAction={ON_CLICK}
+        onIncreaseClick={INCREASE_ACTION}
+        onDecreaseClick={DECREASE_ACTION}
+      >
+        Click
+      </Stepper>
+    )
+  })
+
   xit('Renders its children', () => {
     const tree = renderer
       .create(<Stepper>👶👶🏻👶🏼👶🏽👶🏾👶🏿</Stepper>)
@@ -14,10 +30,23 @@ describe('Stepper', () => {
   })
 
   it('Should run the OnClickAction passed as a prop when clicked', () => {
-    const stepper = shallow(<Stepper onClickAction={ON_CLICK}>Click</Stepper>)
-
     stepper.simulate('click')
-
     expect(ON_CLICK).toHaveBeenCalled()
+  })
+
+  it('Should run the onIncreaseClick action when the inc button is clicked ', () => {
+    stepper.find('#mantecao-stepper-inc').simulate('click')
+    expect(INCREASE_ACTION).toBeCalled()
+  })
+
+  it('Should run the onDecreaseClick action when the inc button is clicked ', () => {
+    stepper.find('#mantecao-stepper-dec').simulate('click')
+    expect(DECREASE_ACTION).toBeCalled()
+  })
+
+  it('Should not run the onClickAction action when the inc button is clicked ', () => {
+    ON_CLICK.mockClear()
+    stepper.find('#mantecao-stepper-inc').simulate('click')
+    expect(ON_CLICK).not.toBeCalled()
   })
 })
