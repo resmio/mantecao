5187d9360e6bb125a40e1baa456d91a9bcf24462
Steven Hein <steven.c.hein@gmail.com>
2017-08-23 18:02:10 +0200

    Add a Modal from popover

---
+++ src/Modal/Modal.stories.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import Modal from './Modal'
+
+storiesOf('Modal', module).add('default', () =>
+  <Modal show onClose={action('onClose')}>
+    <div style={{ background: '#FFF', padding: '1em' }}>
+      dadaf
+    </div>
+  </Modal>
+)
--- src/Popover/Popover.stories.js
+++ src/Popover/Popover.stories.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { findDOMNode } from 'react-dom'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import Popover from './Popover'
